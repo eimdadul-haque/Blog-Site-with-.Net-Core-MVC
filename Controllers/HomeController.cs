413b0e9a0fcafc0eb21671e3012f6fa4966808c5
eimdadul <eimdadulhaque@gmail.com>
2021-04-26 22:22:17 +0600

    Add first controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog_Site_Core.Controllers
+{
+    public class HomeController : Controller
+    {
+       public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult post()
+        {
+            return View();
+        }
+    }
+}
